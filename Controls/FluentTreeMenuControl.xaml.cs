a16f1925368080c3bc4d774ffd81fc4a7fe1f6b3
AndreyXP <andreyapple99@mail.ru>
2026-06-05 15:17:18 +0300

    Добавьте файлы проекта.

---
+++ Controls/FluentTreeMenuControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FluentTreeMenu.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для FluentTreeMenuControl.xaml
+    /// </summary>
+    public partial class FluentTreeMenuControl : UserControl
+    {
+        public FluentTreeMenuControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
